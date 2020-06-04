375148f503ad3e4393be0243c5870a069d355b4b
Haewhybabs <babalolaisaac@gmail.com>
2020-06-04 21:53:13 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+var expressValidator = require("express-validator");
+const userRouter = require('./routes/users.js');
+const eventRouter = require('./routes/events.js');
+var keys = require('./key');
+const app = express();
+//Create Connection
+mongoose.connect(keys.mongoURI);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/uploads', express.static('uploads'));
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//validator
+app.use(
+    expressValidator({
+        errorFormatter: function(param, msg, value) {
+            var namespace = param.split("."),
+                root = namespace.shift(),
+                formParam = root;
+
+            while (namespace.length) {
+                formParam += "[" + namespace.shift() + "]";
+            }
+            return {
+                param: formParam,
+                msg: msg,
+                value: value
+            };
+        }
+    })
+);
+
+
+app.use('/user', userRouter);
+app.use('/event', profileRouter);
+
+port = process.env.PORT || 8000;
+app.listen(port, function() {
+    console.log("server started on port :" + port);
+});
+
+module.exports = app;
